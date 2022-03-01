7040aa57e5979b46ba07cc17f77ab7b41013edb7
Pablo <45538982+Pabloitl@users.noreply.github.com>
2022-03-01 15:53:53 -0600

    feat: Implement gradient descent with sympy

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# ai-gradient-descent
-First exam for Artifitial Intelligence course
+## Gradient descent with sympy
+
+Build with:
+- `python`
+- `sympy`
+
+## How to run
+
+```sh
+python main.py
+```
+
+### Sample input
+
+For dependent variables:
+- `x y`
+
+For function:
+- `4 * x + 6 * y - 2 * x ** 2 - 2 * x * y - 2 * y ** 2`
---
+++ main.py
@@ -0,0 +1,42 @@
+from sympy import (
+    symbols,
+    lambdify,
+    derive_by_array,
+    Matrix,
+    parse_expr
+)
+
+
+def find_max(f, syms, h: float = 1e-3, eps: float = 1e-3):
+    variables = symbols(syms)
+    grad_f = lambdify(variables, derive_by_array(f, variables))
+    result = Matrix([1] * len(variables))
+
+    print(grad_f)
+
+    for _ in range(100):
+        grad_value = Matrix(grad_f(*result))
+
+        if grad_value.norm() < eps:
+            break
+
+        result += grad_value * h
+
+        print(grad_value, grad_value.norm())
+
+    return (result[0], result[1])
+
+
+def main():
+    syms: str = input('Write independent variables to use separated spaces\n→ ')
+    expr: str = input('Write function with python syntax\n→ ')
+
+    try:
+        f = parse_expr(expr)
+
+        print(f'\nResult → {find_max(f, syms, (1/4), .02)}')
+    except Exception as _:
+        print(f'\nCannot parse → "{expr}" (Check your input)')
+
+if __name__ == "__main__":
+    main()
