8289871ed267880ebb45403c656c7a96b7af838c
Pablo <45538982+Pabloitl@users.noreply.github.com>
2022-03-02 10:46:47 -0600

    feat: Extract iterations to argument

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ from sympy import (
 )
 
 
-def find_max(f, syms, h: float = 1e-3, eps: float = 1e-3):
+def find_max(f, syms, h: float = 1e-3, eps: float = 1e-3, iterations: int = 100):
     variables = symbols(syms)
     grad_f = lambdify(variables, derive_by_array(f, variables))
     result = Matrix([1] * len(variables))
 
-    for _ in range(100):
+    for _ in range(iterations):
         grad_value = Matrix(grad_f(*result))
 
         if grad_value.norm() < eps:
@@ -30,7 +30,7 @@ def main():
     try:
         f = parse_expr(expr)
 
-        print(f'\nResult → {find_max(f, syms)}')
+        print(f'\nResult → {find_max(f, syms, iterations=10000)}')
     except Exception as _:
         print(f'\nCannot parse → "{expr}" (Check your input)')
 
